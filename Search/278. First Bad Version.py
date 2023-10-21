ec303bd19178e81a92b59c7e9acc5a5b7f4d438f
bnoora <bnooranii@gmail.com>
2023-10-21 12:53:50 -0700

    1

---
+++ Search/278. First Bad Version.py
@@ -0,0 +1,16 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low = 1
+        high = n
+        prev = True
+        previ = 0
+        while low <= high:
+            mid = (low + high) // 2
+            guess = isBadVersion(mid) 
+            if guess == True:
+                prev = guess
+                previ = mid
+                high = mid - 1
+            else:
+                low = mid + 1 
+        return previ
