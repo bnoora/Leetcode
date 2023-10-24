370465a6e5160a375b14b1b5f54127bd943eb766
bnoora <bnooranii@gmail.com>
2023-10-23 17:59:36 -0700

    more heaps

--- Heaps/1046. Last Stone Weight.py
+++ Heaps/1046. Last Stone Weight.py
@@ -1,4 +1,34 @@
+import heapq
+
 class Solution:
-    def lastStoneWeight(self, stones: [int]) -> int:
-        pass
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stonesheap = MaxHeap()
+        for i in stones:
+            stonesheap.push(i)
         
+        while stonesheap.size() > 1:
+            a = stonesheap.pop()
+            b = stonesheap.pop()
+            if a != b:
+                stonesheap.push(a-b)
+            if stonesheap.size() == 0:
+                return 0
+        return stonesheap.peek()
+
+        
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, val):
+        heapq.heappush(self.heap, -val)
+
+    def pop(self):
+        return -heapq.heappop(self.heap)
+
+    def peek(self):
+        return -self.heap[0]
+
+    def size(self):
+        return len(self.heap)
--- Heaps/973. K Closest Points to Origin.py
+++ Heaps/973. K Closest Points to Origin.py
@@ -1,3 +1,17 @@
+import math
+import heapq
+
 class Solution:
     def kClosest(self, points: [[int]], k: int) -> [[int]]:
-        pass
+        res = []
+        pointsheap = []
+        for i in points:
+            pointsheap.append((math.sqrt(i[0]**2 + i[1]**2), i))
+        heapq.heapify(pointsheap)
+        for i in range(k):
+            i, val = heapq.heappop(pointsheap)
+            res.append(val)
+        return res
+
+
+
