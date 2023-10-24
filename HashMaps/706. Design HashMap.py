73da46189411227ac099c77fd7796df31c0b8e25
bnoora <bnooranii@gmail.com>
2023-10-24 16:41:00 -0700

    more hashhh

--- HashMaps/706. Design HashMap.py
+++ HashMaps/706. Design HashMap.py
@@ -2,71 +2,60 @@ class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.next = None
 
 class MyHashMap:
 
     def __init__(self):
-        self.size = 0
-        self.capacity = 2
-        self.hashset = [None] * self.capacity
+        self.size = 1000
+        self.hashmap = [None] * self.size
+
+    def hash(self, key):
+        return key % self.size
 
     def put(self, key: int, value: int) -> None:
         index = self.hash(key)
-
-        while True:
-            if self.hashset[index] is None:
-                self.hashset[index] = Pair(key, value)
-                self.size += 1
-                if self.size >= self.capacity // 2:
-                    self.rehash()
-                return
-            elif self.hashset[index].key == key:
-                self.hashset[index].value = value
-                return
-            
-            index = (index + 1) % self.capacity
-
-
-
-
-    def remove(self, key: int) -> None:
-        if not self.get(key):
-            return
-        
-        index = self.hash(key)
-        while True:
-            if self.hashset[index].key == key:
-                self.map[index] = None
-                self.size -= 1
-                return
-            index += 1
-            index = index % self.capacity
+        if self.hashmap[index] == None:
+            self.hashmap[index] = Pair(key, value)
+        else:
+            curr = self.hashmap[index]
+            while True:
+                if curr.key == key:
+                    curr.value = value
+                    return
+                if curr.next == None:
+                    break
+                curr = curr.next
+            curr.next = Pair(key, value)
         
 
-    def get(self, key: int) -> bool:
+    def get(self, key: int) -> int:
         index = self.hash(key)
-        while self.hashset[index] is not None:
-            if self.hashset[index].key == key:
-                return self.hashset[index].value
-            index = (index + 1) % self.capacity
+        curr = self.hashmap[index]
+        while curr:
+            if curr.key == key:
+                return curr.value
+            curr = curr.next
         return -1
 
-    def hash(self, key):
-        charidx = 0
-        key = str(key)
-        for i in key:
-            charidx += ord(i)
-        return charidx % self.capacity
-    
-    def rehash(self):
-        self.capacity *= 2
-        newhashset = [None] * self.capacity
-
-        oldhashset = self.hashset
-        self.hashset = newhashset
-        self.size = 0
-        for p in oldhashset:
-            if p is not None:
-                self.put(p.key, p.value)
         
+    def remove(self, key: int) -> None:
+        index = self.hash(key)
+        curr = self.hashmap[index]
+        prev = self.hashmap[index-1]
+
+        if curr == None:
+            return
+        if curr.key == key:
+            self.hashmap[index] = curr.next
+        else:
+            while curr.next:
+                if curr.next.key == key:
+                    curr.next = curr.next.next
+                    return
+                curr = curr.next
+                
+        
+
+
 
