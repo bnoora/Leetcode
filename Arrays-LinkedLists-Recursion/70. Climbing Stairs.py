572ef7a87e7f5fbea83e137220090d91e89cb65d
bnoora <bnooranii@gmail.com>
2023-10-28 13:29:20 -0700

    1

--- Arrays-LinkedLists-Recursion/70. Climbing Stairs.py
+++ Arrays-LinkedLists-Recursion/70. Climbing Stairs.py
@@ -5,3 +5,15 @@ class Solution:
         for i in range(n-1):
             last, last2 = last + last2, last
         return last
+    
+class Solution2:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        dp = [1,2]
+        for i in range(2,n):
+            new = dp[0] + dp[1]
+            dp[0] = dp[1]
+            dp[1] = new
+        return dp[1]
